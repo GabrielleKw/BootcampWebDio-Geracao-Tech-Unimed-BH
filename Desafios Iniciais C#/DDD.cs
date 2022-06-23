feb57f71cfed51329b86a863827246f6ee084f54
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-06-23 09:52:14 -0300

    Create DDD.cs

---
+++ Desafios Iniciais C#/DDD.cs
@@ -0,0 +1,30 @@
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+using System; 
+
+class minhaClasse
+{
+  static void Main(string[] args) 
+  { 
+    int ddd = int.Parse(Console.ReadLine());
+//TODO: Complete os espaços em branco com uma possível solução para o problema
+           if (ddd == 61)
+                Console.WriteLine("Brasilia");
+            else if (ddd == 71)
+                Console.WriteLine("Salvador");
+            else if (ddd == 11)
+                Console.WriteLine("Sao Paulo");
+            else if (ddd == 21)
+                Console.WriteLine("Rio de Janeiro");
+            else if (ddd == 32)
+                Console.WriteLine("Juiz de Fora");
+            else if (ddd == 19)
+                Console.WriteLine("Campinas");
+            else if (ddd == 27)
+                Console.WriteLine("Vitoria");
+            else if (ddd == 31)
+                Console.WriteLine("Belo Horizonte");
+            else
+                Console.WriteLine("DDD nao cadastrado");
+  }
+}
