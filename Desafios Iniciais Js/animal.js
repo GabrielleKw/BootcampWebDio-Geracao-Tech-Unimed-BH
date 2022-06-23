bea40145a45d010ae8a9146921afd402e5b94370
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-06-23 09:47:15 -0300

    Create animal.js

---
+++ Desafios Iniciais Js/animal.js
@@ -0,0 +1,40 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída(output) de dados.
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+let x = gets(); 
+let y = gets(); 
+let z = gets(); 
+
+if ((x == "vertebrado") && (y == "ave")  && (z == "carnivoro")) {
+  print("aguia\n");
+}
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+if ((x == "vertebrado") && (y == "ave")  && (z == "onivoro")) {
+  print("pomba\n");
+}
+
+if ((x == "vertebrado") && (y == "mamifero")  && (z == "onivoro")) {
+  print("homem\n");
+}
+
+if ((x == "vertebrado") && (y == "mamifero")  && (z == "herbivoro")) {
+  print("vaca\n");
+}
+
+if ((x == "invertebrado") && (y == "inseto")  && (z == "hematofago")) {
+  print("pulga\n");
+}
+
+if ((x ==  "invertebrado") && (y == "inseto")  && (z == "herbivoro")) {
+  print("lagarta\n");
+}
+
+if ((x  == "invertebrado") && (y == "anelideo") && (z == "hematofago")) {
+  print("sanguessuga\n");
+}
+
+if ((x  == "invertebrado") && (y == "anelideo") && (z == "onivoro")) {
+  print("minhoca\n");
+}
