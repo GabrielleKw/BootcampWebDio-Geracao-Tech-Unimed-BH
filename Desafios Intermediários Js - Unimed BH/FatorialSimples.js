781fbca259193c219b023f0f52ac774adb3b6855
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-06-23 09:39:49 -0300

    Create FatorialSimples.js

---
+++ Desafios Intermediários Js - Unimed BH/FatorialSimples.js
@@ -0,0 +1,14 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída(output) de dados.
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+let N = parseInt(gets());
+let fatorial = 1;
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+      
+for( i = N - 1; i > 0; i --){
+      N = N * i;
+      fatorial = N
+}
+      
+print(fatorial);
