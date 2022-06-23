e52955287eeec4d171768b6104edff6bf9dc9644
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-06-23 09:45:30 -0300

    Create  DDD.js

---
+++ Desafios Iniciais Js/DDD.js
@@ -0,0 +1,15 @@
+const DDD = parseInt(gets());
+
+//TODO: Complete os espaços em branco com uma possível solução para o problema
+
+switch (DDD) {
+	case 61: print("Brasilia"); break;
+	case 71: print("Salvador"); break;
+	case 11: print("Sao Paulo"); break;
+	case 21: print("Rio de Janeiro"); break;
+	case 32: print("Juiz de Fora"); break;
+	case 19: print("Campinas"); break;
+	case 27: print("Vitoria"); break;
+	case 31: print("Belo Horizonte"); break;
+    default: print("DDD nao cadastrado");
+}
