ba9fa9ac3fd52615e8de5aaa8e5244fe20e0c4fb
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-06-23 09:41:38 -0300

    Create Divisores1.js

---
+++ Desafios Intermediários Js - Unimed BH/Divisores1.js
@@ -0,0 +1,14 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída(output) de dados.
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+let n = parseInt(gets());
+
+//TODO: Complete os espaços em branco com uma possível solução para o problema
+
+for (let i = 1;  i<=n; i++) 
+{
+    if (n % i == 0) 
+    {
+        print(i);
+    }
+}
